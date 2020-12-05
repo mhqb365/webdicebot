79fe6222469fe9e1ee0b519d064a8132cdbaaa58
mhqb365 <mhqb365@gmail.com>
2020-12-05 15:53:19 +0700

    new-deploy

--- js/index.js
+++ js/index.js
@@ -258,7 +258,7 @@ function recoverLicense() {
 $(document).ready(() => {
     new ClipboardJS('.btn');
 
-    // $("#myModal").modal()
+    $("#myModal").modal()
 
     var selectBot = bots.map(bot => `<option value="${bot.name}">${bot.name}</option>`)
 
