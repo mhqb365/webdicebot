759349b2f9785df8ba3cf2286492c7607cf76ae2
mhqb365 <mhqb365@gmail.com>
2021-01-01 09:17:52 +0700

    new-deploy

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname == 'localhost' ? 'http://localhost:8000' : 'https://apiold.webdicebot.xyz'
+const API_URL = window.location.hostname == 'localhost' || window.location.hostname == '127.0.0.1' ? 'http://localhost:8000' : 'https://apiold.webdicebot.xyz'
 
 const bots = [
     {
